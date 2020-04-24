12bdcfe27b4f138160074b031453da6d3d5103ba
clemence darriet <cdarriet@g.ucla.edu>
2020-04-24 15:05:19 -0700

    to merge updated script

--- transcripts.R
+++ transcripts.R
@@ -7,6 +7,17 @@ getwd()
 transcripts17<-read_csv("UCLA_CS_CLASS_OF_2017_NO NAMES_NO IDs.csv")
 view(transcripts17)
 
-transcripts17<-transcripts17 %>% select(5:12)
+#create a cohort variable
+transcripts17 %>% count()
+names(transcripts17)
+transcripts17 %>% group_by(CourseID,CourseTitle) %>% arrange(CourseID) %>% count() %>% view()
+
+transcripts17 %>% group_by(CourseID,CourseTitle) %>%
+  filter(SchoolAttended=="RFK Comm Schls - UCLA Com")  %>% 
+  arrange(CourseID) %>% count() %>% view()
+
+#homegrown
 
 #we did it! yay!
+
+#Ariana make an A-G
